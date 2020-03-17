ea22eb1d314a014869c5663257fb30a9b70635fe
SHUcream00 <requiemdeciel@gmail.com>
2020-03-17 15:24:19 +0900

    1

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ Requirement:
 Python 3.7 or up, discord-py, BeautifulSoup4, pil
 
 '''
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 @client.event
 async def on_ready():
-    client.loop.create_task(bluebird())
-    client.loop.create_task(saryunan())
-    client.loop.create_task(mandubird())
+    #client.loop.create_task(bluebird())
+    #client.loop.create_task(saryunan())
+    #client.loop.create_task(mandubird())
     print('Eurika NEXT Main activated')
     print(client.user.name)
     print('------')
