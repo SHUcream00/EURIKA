057e636ef71c01dc501cd3a3ea427c9af975449b
SHUcream00 <requiemdeciel@gmail.com>
2020-03-27 17:44:33 +0900

    hierachy adjustment

---
+++ eurika/__init__.py
@@ -0,0 +1,14 @@
+
+"""
+EURIKA
+===============================================
+A basic support bot for Discord
+:copyright: (c) 2009-2020 SHUcream
+:license: MIT, see LICENSE for more details.
+===============================================
+
+__title__ = 'Eurika'
+__author__ = 'SHUcream'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2009-2020 SHUcream'
+__version__ = '2.0.0'
