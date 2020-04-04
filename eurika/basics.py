c624f39e8a4c8450c6b7abf8386f84e8f840b747
SHUcream00 <requiemdeciel@gmail.com>
2020-04-04 18:52:25 +0900

    modules

--- eurika/basics.py
+++ eurika/basics.py
@@ -1,18 +1,32 @@
+import re
 import random
 import datetime
 import aiohttp
+import ast
+from math import *
+
+#__import__('os').system('rm -rf /dir')
 
 async def rand_select(selections):
     '''return a random element from iterables.'''
     return random.choice(selections)
 
-async def dice_roll(times, head, adjust=0): -> int
+async def dice_roll(times, head, adjust=0):
     '''NdN dice, if NdN is not given'''
     return times * head + adjust
 
-async def dday(first: datetime.date, second:datetime.date): -> int
+async def dday(first: datetime.date, second:datetime.date):
     '''Tells difference between two given dates in days'''
     return (first-second).days
+
+def calc(exp:str):
+    #THIS USES EVAL PERIOD
+    '''Calculate the given expression without eval()'''
+    math_funcs = {'tan':'', 'sin':'','cos':'','acos':'','asin':'','atan':'','pi':'','e':'',\
+                'tau':'', 'inf':'', 'nan':''}
+    exp = exp.replace('^', '**').replace('x','*')
+    #eval(exp, {"__builtins__":None}, math_funcs)
+    return eval(exp)
 
 async def jaegi():
     '''Get Han River's current temperature'''
@@ -20,10 +34,19 @@ async def jaegi():
         async with aiohttp.ClientSession() as session:
             return json.loads(await jaegi.text())['temp']
 
-async def upside_dn(alnum):
+async def upside_dn(alnum: str):
+    #unfinished - unicode related problem
     '''Prints text upside down, should be alphabet or arabic numbers'''
     alts = {'a':'ɐ', 'b':'q', 'c':'ɔ', 'd':'p', 'e':'ǝ', 'f':'ɟ', 'g':'ƃ', 'h':'ɥ', \
             'i':'ᴉ', 'j':'ɾ', 'k':'ʞ', 'l':'l', 'm':'ɯ', 'n':'u', 'o':'o', 'p':'d', \
             'q':'b', 'r':'ɹ', 's':'s', 't':'ʇ', 'u':'n', 'v':'ʌ', 'w':'ʍ', 'x':'x', \
-            'y':'ʎ', 'z':'z', 1:'Ɩ', 2:'ᄅ', 3:'Ɛ', 4:'ㄣ', 5:'ϛ', 6:'9', 7:'ㄥ', 8:'8', 9:'6'}
-    return ''.join
+            'y':'ʎ', 'z':'z', '1':'Ɩ', '2':'ᄅ', '3':'Ɛ', '4':'ㄣ', '5':'ϛ', '6':'9', \
+            '7':'ㄥ', '8':'8', '9':'6'}
+    return list(alts.get(i, 'X') for i in alnum[::-1])
+
+#print(calc("1+2+32"))
+#print(calc("32 // 5"))
+print(calc("tan(60)"))
+#print(calc("pi"))
+#print(calc("inf"))
+print(calc("print(version())"))
