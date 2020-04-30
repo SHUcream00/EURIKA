b39cd0deb36caf7ff060048c8cf4bfbc723a3c8b
SHUcream00 <requiemdeciel@gmail.com>
2020-04-30 15:31:44 +0900

    fix hototogisu bugs

--- eurika/ext/cuckoo.py
+++ eurika/ext/cuckoo.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function
 
 import json
 import tweepy
-import discord
+import ast
 import sqlite3
 
 cwd = r'C:\EurikaMkIII' #Do I really need this? can't i use cwd()?
@@ -13,40 +13,38 @@ description = '''Python Twitter wrapper for EURIKA'''
 class hototogisu():
 
     def __init__(self):
-        consumer_key="KEY"
-        consumer_secret="SECRET"
-        access_token="TOKEN"
-        access_token_secret="TOKEN_SECRET"
+        self.consumer_key="KEY"
+        self.consumer_secret="SECRET"
+        self.access_token="TOKEN"
+        self.access_token_secret="TOKEN_SECRET"
 
-        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
-        auth.set_access_token(self.access_token, self.access_token_secret)
+        self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_token, self.access_token_secret)
 
-    async def cry(self, **kwargs):
+    def cry(self, **kwargs):
         api = tweepy.API(self.auth)
         twtid = kwargs['ID']
         twtdet = api.user_timeline(id = twtid, count=10, tweet_mode='extended')
         twtres = []
-        twtprv = ''
         db = sqlite3.connect(cwd + '\db\EurDB.db')
         cursor = db.cursor()
+        twtcur = set()
+
         for i, j in enumerate(twtdet):
             twtcache = cursor.execute("SELECT * FROM TwitterCache WHERE ID='"+str(twtid)+"' COLLATE NOCASE").fetchone()
             twttemp = []
             j = j._json
 
-            twttemp.append(j['id'])
-            twttemp.append(j['full_text'])
-            twttemp.append(j['user']['name'])
-            twttemp.append(j['user']['profile_image_url'])
-            twttemp.append(j['user']['profile_sidebar_border_color'])
+            twttemp.extend([j['id'], j['full_text'], j['user']['name'], j['user']['profile_image_url'],\
+                            j['user']['profile_sidebar_border_color']])
+
             if 'profile_banner_url' in j['user']:
                 twttemp.append(j['user']['profile_banner_url'])
             else:
                 twttemp.append(None)
             twttemp.append(j['created_at'])
 
-            twtprv += ' | ' + str(twttemp[0])
-
+            twtcur.add(twttemp[0])
 
             if 'media' in j['entities']:
                 twttemp.append(j['entities']['media'][0]['media_url'])
@@ -54,9 +52,7 @@ class hototogisu():
                 twttemp.append(None)
             twttemp.append(twtcache[4])
 
-            if str(twttemp[0]) not in twtcache[2] and len(twtcache[2].split('|')) == 11:
-                #print('<<Anomalyfound>>\n' + str(kwargs['ID']) + '\n', 'previous:\n' + str(twtcache[2]) + '\n', 'new:\n' + str(twttemp[0]))
-                #print('length:' + str(len(twtcache[2].split('|'))))
+            if twttemp[0] not in ast.literal_eval(twtcache[2]):
                 twtres.append(twttemp)
 
             if i == 0:
@@ -65,7 +61,18 @@ class hototogisu():
                     twtres.append(twtpfupdate)
                 cursor.execute("UPDATE TwitterCache SET Last=? Where ID=?", (j['full_text'], twtid))
 
-        cursor.execute("UPDATE TwitterCache SET Previous=?, Profile=? Where ID=?", (twtprv, j['user']['profile_image_url'], twtid))
+        twtprv = ast.literal_eval(cursor.execute("SELECT * FROM TwitterCache WHERE ID='"+str(twtid)+"' COLLATE NOCASE").fetchone()[2])
+        cursor.execute("UPDATE TwitterCache SET Previous=?, Profile=? Where ID=?", (str(twtprv.union(twtcur)), j['user']['profile_image_url'], twtid))
         db.commit()
         db.close()
         return twtres
+
+'''
+if __name__ == "__main__":
+    db = sqlite3.connect(cwd + '\db\EurDB.db')
+    a = hototogisu()
+    #a.cry()
+    for j in db.cursor().execute("SELECT ID FROM TwitterCache").fetchall():
+        print(j)
+        a.cry(ID=j[0])
+'''
