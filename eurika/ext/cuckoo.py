56dd41fac29e0f5d033c79f4cba4f32a6fdaa3d1
SHUcream00 <requiemdeciel@gmail.com>
2020-05-01 12:57:49 +0900

    no active connection issue solve

--- eurika/ext/cuckoo.py
+++ eurika/ext/cuckoo.py
@@ -4,6 +4,7 @@ import json
 import tweepy
 import ast
 import aiosqlite
+import asyncio
 
 cwd = r'C:\EurikaMkIII' #Do I really need this? can't i use cwd()?
 
@@ -17,6 +18,12 @@ class hototogisu():
         self.consumer_secret="SECRET"
         self.access_token="TOKEN"
         self.access_token_secret="TOKEN_SECRET"
+
+        self.consumer_key="ORQyianaloyZ8TMB6acRvr2fV"
+        self.consumer_secret="8gW8zLVjYpVhEbZHDhilWwZWh0BMAC4H71wwf6D0MHKNJydShZ"
+        self.access_token="3752069838-Wpgda1YLKhFU85chnlCbePYHprmJtyAYGYcllR8"
+        self.access_token_secret="YiNfGLfqKcTGNLo9zCII5b3hzjU9ExaEdZLqPMhmCapZ9"
+
         self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
         self.auth.set_access_token(self.access_token, self.access_token_secret)
 
@@ -26,52 +33,56 @@ class hototogisu():
         twtdet = api.user_timeline(id = twtid, count=10, tweet_mode='extended')
         twtres = []
         async with aiosqlite.connect(cwd + "\db\EurDB.db") as db:
-            twtcur = set()
-            cursor = await db.execute("SELECT * FROM TwitterCache WHERE ID='"+str(twtid)+"' COLLATE NOCASE")
-            twtcache = await cursor.fetchone()
-            for i, j in enumerate(twtdet):
-                twttemp = []
-                j = j._json
+            async with db.execute("SELECT * FROM TwitterCache WHERE ID='"+str(twtid)+"' COLLATE NOCASE") as cursor:
 
-                twttemp.extend([j['id'], j['full_text'], j['user']['name'], j['user']['profile_image_url'],\
-                                j['user']['profile_sidebar_border_color']])
+                twtcur = set()
+                twtcache = await cursor.fetchone()
 
-                if 'profile_banner_url' in j['user']:
-                    twttemp.append(j['user']['profile_banner_url'])
-                else:
-                    twttemp.append(None)
-                twttemp.append(j['created_at'])
+                for i, j in enumerate(twtdet):
+                    twttemp = []
+                    j = j._json
 
-                twtcur.add(twttemp[0])
+                    twttemp.extend([j['id'], j['full_text'], j['user']['name'], j['user']['profile_image_url'],\
+                                    j['user']['profile_sidebar_border_color']])
 
-                if 'media' in j['entities']:
-                    twttemp.append(j['entities']['media'][0]['media_url'])
-                else:
-                    twttemp.append(None)
-                twttemp.append(twtcache[4])
+                    if 'profile_banner_url' in j['user']:
+                        twttemp.append(j['user']['profile_banner_url'])
+                    else:
+                        twttemp.append(None)
+                    twttemp.append(j['created_at'])
 
-                if twttemp[0] not in ast.literal_eval(twtcache[2]):
-                    twtres.append(twttemp)
+                    twtcur.add(twttemp[0])
 
-                if i == 0:
-                    if twtcache[5].strip() != j['user']['profile_image_url'].strip():
-                        twtpfupdate = [j['id'], j['user']['name'] + '의 프사가 변경되었습니다.', j['user']['name'], twtcache[5], j['user']['profile_sidebar_border_color'], None, j['created_at'], j['user']['profile_image_url'].replace('_normal', ''), twtcache[4]]
-                        twtres.append(twtpfupdate)
-                    await db.execute("UPDATE TwitterCache SET Last=? Where ID=?", (j['full_text'], twtid))
+                    if 'media' in j['entities']:
+                        twttemp.append(j['entities']['media'][0]['media_url'])
+                    else:
+                        twttemp.append(None)
+                    twttemp.append(twtcache[4])
 
-        twtprv = ast.literal_eval(await cursor.execute("SELECT * FROM TwitterCache WHERE ID='"+str(twtid)+"' COLLATE NOCASE").fetchone()[2])
-        await db.execute("UPDATE TwitterCache SET Previous=?, Profile=? Where ID=?", (str(twtprv.union(twtcur)), j['user']['profile_image_url'], twtid))
-        await db.commit()
-        await db.close()
-        return twtres
+                    if twttemp[0] not in ast.literal_eval(twtcache[2]):
+                        twtres.append(twttemp)
+
+                    if i == 0:
+                        if twtcache[5].strip() != j['user']['profile_image_url'].strip():
+                            twtpfupdate = [j['id'], j['user']['name'] + '의 프사가 변경되었습니다.', j['user']['name'], twtcache[5], j['user']['profile_sidebar_border_color'], None, j['created_at'], j['user']['profile_image_url'].replace('_normal', ''), twtcache[4]]
+                            twtres.append(twtpfupdate)
+                        await db.execute("UPDATE TwitterCache SET Last=? Where ID=?", (j['full_text'], twtid))
+
+
+                twtprv = ast.literal_eval(twtcache[2])
+                await db.execute("UPDATE TwitterCache SET Previous=?, Profile=? Where ID=?", (str(twtprv.union(twtcur)), j['user']['profile_image_url'], twtid))
+                await db.commit()
+                #await db.close()
+                return twtres
 
 
 if __name__ == "__main__":
     b = asyncio.get_event_loop()
     a = hototogisu()
-    #a.cry()
+    b.run_until_complete(a.cry(ID=2968069742))
+    '''
     async with aiosqlite.connect(cwd + "\db\EurDB.db") as db:
         async with db.execute("SELECT ID FROM TwitterCache") as cursor:
             for j in await cursor.fetchall():
                 print(j)
-                b.create_task(a.cry(ID=j[0]))
+                b.create_task(a.cry(ID=j[0]))'''
