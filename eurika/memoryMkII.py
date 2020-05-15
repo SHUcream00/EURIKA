71a96dabc200c74dca85cc247c6e0ab5ae3e34bd
SHUcream00 <requiemdeciel@gmail.com>
2020-05-15 15:50:50 +0900

    Fixed all the bugs

--- eurika/memoryMkII.py
+++ eurika/memoryMkII.py
@@ -9,45 +9,44 @@ Author: 2020 SHUcream
 '''
 
 class memento():
-    cwd = r'C:\EurikaMkIII'
 
-    def __init__(self, db):
+    def __init__(self):
+        cwd = r'C:\EurikaMkIII' #fallback
         self.db = cwd + "\db\EurDB.db"
 
     async def recall(self, key):
         async with aiosqlite.connect(self.db) as db:
-            async with db.execute("SELECT * FROM Memory WHERE Name='"+key+"' COLLATE NOCASE") as cursor:
+            async with db.execute(f"SELECT * FROM Memory WHERE Name='{key}' COLLATE NOCASE") as cursor:
                 res = await cursor.fetchone()
                 text_color = 0x07ECBA
 
                 if res == None:
-                    async with db.execute("SELECT * FROM Memory WHERE Name Like'%"+key+"%' COLLATE NOCASE") as cursor:
+                    async with db.execute(f"SELECT * FROM Memory WHERE Name Like'%{key}%' COLLATE NOCASE") as cursor:
                         res = list(map(lambda x: x[1], await cursor.fetchall()))
                         text_color = 0xB5002B
 
                 return res, text_color
 
-    async def memorize(key, value, **kwargs):
+    async def memorize(self, key, value, **kwargs):
         async with aiosqlite.connect(self.db) as db:
-            async with db.execute("SELECT * FROM Memory WHERE Name='"+key+"' COLLATE NOCASE") as cursor:
+            async with db.execute(f"SELECT * FROM Memory WHERE Name='{key}' COLLATE NOCASE") as cursor:
                 res = await cursor.fetchone()
                 if res == None:
                     return False, 0xB5002B
                 else:
-                    await db.execute("INSERT INTO Memory (Name, Value, User, Date) VALUES ('" + key + "', '" + value + "', '"+ kwargs['author'] +"', '"+ time.strftime('%Y-%m-%d %H:%M:%S') +"')")
+                    await db.execute(f"INSERT INTO Memory (Name, Value, User, Date) VALUES ('{key}', '{value}', '{kwargs['author']}', '{time.strftime('%Y-%m-%d %H:%M:%S')}'")
                     await db.commit()
-                    await db.close()
                     return True, 0x07ECBA
 
     async def find(self, key):
         async with aiosqlite.connect(self.db) as db:
-            async with db.execute("SELECT * FROM Memory WHERE Name Like'%"+key+"%' COLLATE NOCASE") as cursor:
+            async with db.execute(f"SELECT * FROM Memory WHERE Name Like'%{key}%' COLLATE NOCASE") as cursor:
                 res = list(map(lambda x: x[1], await cursor.fetchall()))
                 return res, 0xB5002B
 
     async def append(self, key, value, **kwargs):
         async with aiosqlite.connect(self.db) as db:
-            async with db.execute("SELECT * FROM Memory WHERE Name='"+key+"' COLLATE NOCASE") as cursor:
+            async with db.execute(f"SELECT * FROM Memory WHERE Name='{key}' COLLATE NOCASE") as cursor:
                 res = await cursor.fetchone()
                 if res == None:
                     return False, 0xB5002B
@@ -55,12 +54,11 @@ class memento():
                     tgt = (res[2]+" - "+value, kwargs['author'], time.strftime('%Y-%m-%d %H:%M:%S'), key)
                     await db.execute("UPDATE Memory SET value=?, User=?, Date=? Where Name=? COLLATE NOCASE", tgt)
                     await db.commit()
-                    await db.close()
                     return True, 0x07ECBA
 
     async def own(self, **kwargs):
         async with aiosqlite.connect(self.db) as db:
-            async with db.execute("SELECT * FROM Memory WHERE User='"+str(kwargs['author'])+"'") as cursor:
+            async with db.execute(f"SELECT * FROM Memory WHERE User='{kwargs['author']}'") as cursor:
                 res = list(map(lambda x: x[1], await cursor.fetchall()))
                 if res != []:
                     return res, 0x07ECBA
